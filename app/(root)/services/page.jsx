3ba4e175f1ebfb72b7268c5427ddc056857b258d
Rafay <rafaykhan.mesh@gmail.com>
2025-08-18 16:59:25 +0500

    feat: update

--- app/(root)/services/page.jsx
+++ app/(root)/services/page.jsx
@@ -1,8 +1,9 @@
 import AllProductsSummary from "@/components/layout/all-products-summary";
 
-const title = "Explore All";
-const highlightedTitle = "Blogs";
-const subtitle = "Browse all our articles, stories, and updates in one place.";
+const title = "Learn All";
+const highlightedTitle = "Services";
+const subtitle =
+  "Discover our complete range of services tailored to meet your needs.";
 
 export default async function Page() {
   return (
